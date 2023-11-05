3bfbe5b35113b4208e03df4a2e93acc4a81a7628
Hayden Roney <roneyhayden@gmail.com>
2023-11-05 14:52:33 -0600

    Creatediv func, event listeners, styles added

---
+++ index.js
@@ -0,0 +1,25 @@
+document.getElementById("createButton").addEventListener("click", createDivs);
+const userInput = document.getElementById("userInput");
+userInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        createDivs();
+    }
+});
+
+function createDivs() {
+    const userInput = document.getElementById("userInput").value.toUpperCase();
+    const characterContainer = document.getElementById("characterContainer");
+
+    characterContainer.innerHTML = '';
+    const characters = userInput.split('');
+
+    characters.forEach(char => {
+        const divBox = document.createElement("div");
+        divBox.classList.add("character-box");
+        divBox.textContent = char;
+        divBox.addEventListener("click", () => {
+            alert(`You clicked on character: ${char}`);
+        });
+        characterContainer.appendChild(divBox);
+    });
+}
