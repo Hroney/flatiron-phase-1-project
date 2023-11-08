b5a06e7b8c097f82970908e49ea6336c06c39084
Hayden Roney <roneyhayden@gmail.com>
2023-11-08 09:55:22 -0600

    Added data validation to user input

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 document.getElementById("createButton").addEventListener("click", createDivs);
+
 const userInput = document.getElementById("userInput");
+
 userInput.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         createDivs();
@@ -10,8 +12,21 @@ function createDivs() {
     const userInput = document.getElementById("userInput").value.toUpperCase();
     const characterContainer = document.getElementById("characterContainer");
 
+
     characterContainer.innerHTML = '';
     const characters = userInput.split('');
+
+    //I looked up the regex for this portion
+    const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
+    //This is the Test to make sure the word is a single word and not a sentence.
+    if (characters.some(element => element.includes(' '))) {
+        alert(`The word ${userInput} has a space in it`)
+        characters = '';
+    }
+    else if (characters.some(element => punctuation.test(element))) {
+        alert(`The word ${userInput} has punctuation in it`)
+        characters = '';
+    }
 
     characters.forEach(char => {
         const divBox = document.createElement("div");
