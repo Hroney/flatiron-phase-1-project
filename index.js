994b9858c871648b443b9d8352449f383b64808b
Hayden Roney <roneyhayden@gmail.com>
2023-11-08 10:14:08 -0600

    Cleaned up the createDivs() event

--- index.js
+++ index.js
@@ -11,22 +11,23 @@ userInput.addEventListener("keydown", function (event) {
 function createDivs() {
     const userInput = document.getElementById("userInput").value.toUpperCase();
     const characterContainer = document.getElementById("characterContainer");
+    let characters = userInput.split('');
 
-
-    characterContainer.innerHTML = '';
-    const characters = userInput.split('');
-
-    //I looked up the regex for this portion
+    //I looked up the regex for this portion, it should grab every punctuation marker.
     const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
+
     //This is the Test to make sure the word is a single word and not a sentence.
     if (characters.some(element => element.includes(' '))) {
-        alert(`The word ${userInput} has a space in it`)
-        characters = '';
+        alert(`The word/phrase ${userInput} is invalid; it has a space in it.`)
+        characters.length = 0
     }
     else if (characters.some(element => punctuation.test(element))) {
-        alert(`The word ${userInput} has punctuation in it`)
-        characters = '';
+        alert(`The word/phrase ${userInput} is invalid; it has punctuation in it.`)
+        characters.length = 0
     }
+
+    //This portion resets the onscreen div sections
+    characterContainer.innerHTML = '';
 
     characters.forEach(char => {
         const divBox = document.createElement("div");
@@ -37,5 +38,6 @@ function createDivs() {
         });
         characterContainer.appendChild(divBox);
     });
+
     document.getElementById("userInput").value = "";
 }
